4f576b77fe90321ca88f51bc6de1f00cf8ace6de
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-06-03 22:00:03 +0530

    fix: add missing newline for improved readability in seed.js

--- week-14.1(CI-CD)/seed.js
+++ week-14.1(CI-CD)/seed.js
@@ -6,6 +6,7 @@ const DB_NAME = process.env.DB_NAME;
 async function seed() {
   // Get a single, dedicated connection from the pool
   const connection = await pool.getConnection();
+
   try {
     // 1. Corrected Syntax: Added 'S' to EXISTS
     await connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
