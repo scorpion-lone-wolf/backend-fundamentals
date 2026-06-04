c2bf6794b22219808604b0bf1f92c42770f4f9c2
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-06-04 19:05:48 +0530

    fix: comment out unused database queries in blog route

--- week-14.1(CI-CD)/server.js
+++ week-14.1(CI-CD)/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import pool from "./pool.js";
+// import pool from "./pool.js";
 import { getHostName, getVersion } from "./utils/index.js";
 
 const app = express();
@@ -8,10 +8,10 @@ const app = express();
 // handling route
 app.get("/blog", async (req, res) => {
   try {
-    await pool.query(`USE blog`);
-    const [rows] = await pool.query("SELECT * FROM blogs");
+    // await pool.query(`USE blog`);
+    // const [rows] = await pool.query("SELECT * FROM blogs");
     res.json({
-      data: rows,
+      data: "dummy",
       status: "success",
       message: "blog fetched msg",
       version: getVersion(),
